17a4ebeae93ba7fdd6c765c81468a8853bd82f3d
zenkoyana-dotcom <zenkoyana@gmail.com>
2026-07-19 14:01:27 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,11 @@
 
+const SUPABASE_URL = "https://mvudpgtdocgnzntqwwxv.supabase.co";
+
+const SUPABASE_KEY = "ВСТАВЬ_СЮДА_СВОЙ_PUBLISHABLE_KEY";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
+console.log("Supabase подключен");
